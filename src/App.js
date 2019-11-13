4a6246700243bcc3d575963fda2217dc3351d20a
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-11-13 21:13:21 +0000

    refactor: Replace code block to the right place

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
+  
       state = {
       	value: '',
-      };
-      
+      };  
+  
+  render() {    
+    return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
