e5c1e85c929200b34b403183c8475be32f7e07f0
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-11-13 21:18:29 +0000

    feat: Add method to set the state

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  
-      state = {
-      	value: '',
-      };  
+class App extends Component {  
+    state = {
+      value: '',
+    };
+
+	handleChange = event => {
+    	this.setState({ value: event.target.value});
+    };
   
   render() {    
     return (
